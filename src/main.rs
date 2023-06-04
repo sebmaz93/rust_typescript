bd689d9366517d1a5af5b763a3fd1dd67b7d1e4e
sebmaz <titan9389@me.com>
2023-06-04 17:47:28 +0400

    added traits

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,8 @@
 // mod exr_1;
-mod exr_2;
-mod stack_heap;
-
-#[derive(Debug)]
-struct Item {
-    count: usize,
-}
-
-fn add_one(item: &mut Item) {
-    item.count += 1;
-}
-
-fn print_all_items(items: &Vec<Item>) {
-    for item in items {
-        println!("{:?}", item);
-    }
-}
+// mod exr_2;
+// mod stack_heap;
+mod traits;
 
 fn main() {
-    let mut item = Item { count: 1 };
-    println!("{:?}", item);
-
-    add_one(&mut item);
-    println!("{:?}", item);
-
-    let mut items: Vec<Item> = vec![Item { count: 2 }];
-    let first = items.first_mut();
-    println!("{:?}", first);
-
-    print_all_items(&items);
+    traits::main()
 }
